03064b5829de0b7221f34f73b831927051624f9d
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-26 16:39:52 -0300

    feat: req-01 - challenge_study_schedule.py implementado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui"""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    number_of_students = 0
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            number_of_students += 1
+    return number_of_students
