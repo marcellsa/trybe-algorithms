07ce8f5f58a17b9ceeb5fff0dac0f05f1af4d7bc
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-30 17:06:08 -0300

    feat: req-03 - adicionando condição em is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
+
     if word[low_index] != word[high_index]:
         return False
+
+    if len(word) <= 1:
+        return True
+
     if low_index >= high_index:
         return True
 
