20da1430d63eeb3b45cb86bec0661d6957348e98
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-31 15:16:13 -0300

    feat: req-04 - implementando função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,23 @@
+def ordered_string(string):
+    characters = list(string.lower())
+    characters_length = len(characters)
+
+    for i in range(characters_length):
+        min_index = i
+        for j in range(i + 1, characters_length):
+            if characters[j] < characters[min_index]:
+                min_index = j
+        characters[i], characters[min_index] = (
+            characters[min_index],
+            characters[i],
+        )
+    sorted_string = "".join(characters)
+    return sorted_string
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    return ordered_string(first_string) == ordered_string(second_string)
