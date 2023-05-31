8f36dc7d128628a89ae22b20230f8b3358effb06
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-31 17:35:53 -0300

    feat: req-04 - função is_anagram implementada

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,23 +1,47 @@
-def ordered_string(string):
-    characters = list(string.lower())
-    characters_length = len(characters)
+def merge_sort(string):
+    if len(string) <= 1:
+        return string.lower()
 
-    for i in range(characters_length):
-        min_index = i
-        for j in range(i + 1, characters_length):
-            if characters[j] < characters[min_index]:
-                min_index = j
-        characters[i], characters[min_index] = (
-            characters[min_index],
-            characters[i],
-        )
-    sorted_string = "".join(characters)
-    return sorted_string
+    middle = len(string) // 2
+    left_half = string[:middle]
+    right_half = string[middle:]
+
+    ordered_left_half = merge_sort(left_half)
+    ordered_right_half = merge_sort(right_half)
+
+    return merge(ordered_left_half, ordered_right_half)
+
+
+def merge(left_half, right_half):
+    outcome = []
+    index_left = index_right = 0
+
+    while index_left < len(left_half) and index_right < len(right_half):
+        if left_half[index_left] < right_half[index_right]:
+            outcome.append(left_half[index_left])
+            index_left += 1
+        else:
+            outcome.append(right_half[index_right])
+            index_right += 1
+
+    outcome.extend(left_half[index_left:])
+    outcome.extend(right_half[index_right:])
+    sorted_string = "".join(outcome)
+    return sorted_string.lower()
 
 
 def is_anagram(first_string, second_string):
+    ordered_first_string = merge_sort(first_string)
+    ordered_second_string = merge_sort(second_string)
+
     if not first_string or not second_string:
-        return False
+        return (ordered_first_string, ordered_second_string, False)
+
     if len(first_string) != len(second_string):
-        return False
-    return ordered_string(first_string) == ordered_string(second_string)
+        return (ordered_first_string, ordered_second_string, False)
+
+    return (
+        ordered_first_string,
+        ordered_second_string,
+        ordered_first_string == ordered_second_string,
+    )
